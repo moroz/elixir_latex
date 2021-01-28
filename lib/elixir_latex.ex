2333d01123a2c06da66ddbdee8a99cf9ed6d5610
Karol Moroz <k.j.moroz@gmail.com>
2021-01-28 17:36:30 +0800

    Initial commit

---
+++ lib/elixir_latex.ex
@@ -0,0 +1,18 @@
+defmodule ElixirLatex do
+  @moduledoc """
+  Documentation for `ElixirLatex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirLatex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_latex_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirLatexTest do
+  use ExUnit.Case
+  doctest ElixirLatex
+
+  test "greets the world" do
+    assert ElixirLatex.hello() == :world
+  end
+end
