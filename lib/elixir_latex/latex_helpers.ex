f4582cd57ef7d9d95e944629ac05ca47f629baa7
Karol Moroz <k.j.moroz@gmail.com>
2021-01-30 19:29:09 +0800

    Add helper to format plain text as LaTeX

--- lib/elixir_latex/latex_helpers.ex
+++ lib/elixir_latex/latex_helpers.ex
@@ -1,6 +1,7 @@
 defmodule ElixirLatex.LatexHelpers do
   @special_char_regex ~r/([\\\^\%~\#\$%&_\{\}])/
 
+  @spec escape_latex(string :: binary) :: binary
   @doc """
   Replaces all characters that have a special meaning in latex sources
   with their corresponding escape sequences.
@@ -24,5 +25,19 @@ defmodule ElixirLatex.LatexHelpers do
 
   for {source, replacement} <- @replacement_map do
     defp replace_special_char(unquote(source)), do: unquote(replacement)
+  end
+
+  @spec format_plain_as_latex(plain :: iodata) :: binary
+  def format_plain_as_latex(string) when is_binary(string) do
+    string
+    |> String.trim()
+    |> escape_latex()
+    |> String.replace(~r/(\r?\n){2,}/, "\\par{}")
+    |> String.replace(~r/(\r?\n)/, "\\\\\\\\")
+  end
+
+  def format_plain_as_latex(iodata) when is_list(iodata) do
+    IO.iodata_to_binary(iodata)
+    |> format_plain_as_latex()
   end
 end
--- test/elixir_latex/latex_helpers_test.exs
+++ test/elixir_latex/latex_helpers_test.exs
@@ -8,4 +8,18 @@ defmodule ElixirLatex.LatexHelpersTest do
     actual = LatexHelpers.escape_latex(@sample_text)
     assert actual == "\\textbackslash{}documentclass[a4paper]\\{article\\}"
   end
+
+  describe "format_plain_as_latex/1" do
+    @source_paragraph """
+    你好，世界！
+    此為測試段落
+    """
+
+    @expected_paragraph "你好，世界！\\\\此為測試段落"
+
+    test "converts single newlines to LaTeX newline literals" do
+      actual = LatexHelpers.format_plain_as_latex(@source_paragraph)
+      assert actual == @expected_paragraph
+    end
+  end
 end
